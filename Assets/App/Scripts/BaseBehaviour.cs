2584eec4ef7e79081db0967baab103e39cf356cd
hcq0618 <hcq0618@163.com>
2016-07-13 22:25:05 +0800

    add base class
    Signed-off-by: hcq0618 <hcq0618@163.com>

--- Assets/App/Scripts/BaseBehaviour.cs
+++ Assets/App/Scripts/BaseBehaviour.cs
@@ -1,19 +1,31 @@
-﻿using UnityEngine;
+﻿//Created by hcq
+
+using UnityEngine;
 using System.Collections;
 
 public class BaseBehaviour : MonoBehaviour
 {
+	private AndroidJavaObject activity;
 
-	// Use this for initialization
-	void Start ()
+	protected AndroidJavaObject getActivity ()
 	{
-	
+		if (activity == null) {
+			AndroidJavaClass activityClass = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+			activity = activityClass.GetStatic<AndroidJavaObject> ("currentActivity");
+		}
+		return activity;
 	}
-	
-	// Update is called once per frame
-	void Update ()
+
+	protected void runOnAndroidUiThread (Runnable runnable)
 	{
-	
+		getActivity ().Call ("runOnUiThread", new AndroidJavaRunnable (() => {
+			runnable.run ();
+		}));
+	}
+
+	protected interface Runnable
+	{
+		void run ();
 	}
 }
 
